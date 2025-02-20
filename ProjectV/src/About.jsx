2929f948a7ccf7e1357d565e22635c56d9332d39
nadib <codecrafersnadib@gmail.com>
2025-02-21 02:34:24 +0600

    Set up the Router

---
+++ ProjectV/src/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+const About = () => {
+  return (
+<>
+<Header />
+<div>This is the about page</div>
+</>
+
+  )
+}
+
+export default About
---
+++ ProjectV/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+const Home = () => {
+  return (
+    <>
+    <Header />
+       <div>This is the home </div>
+    </>
+  )
+}
+
+export default Home
